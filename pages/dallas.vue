4e759e6e2b3eac22aaf68f47a8570b3a25a1e8d4
J. Scott Smith <scott@newleafsolutionsinc.com>
2017-10-20 00:48:39 -0700

    Added Em, Scott Ambler, reorganized sponsor logos

--- pages/dallas.vue
+++ pages/dallas.vue
@@ -53,7 +53,70 @@
                 Throughout his career, Ahmed has led a number of Fortune 100 companies through large-scale agile transformations using his culture-led transformation approach, which focuses on changing and introducing organizational habits critical to sustaining agility.
               </p>
               <p class="body-copy" key="p2">
-                He is the co-author of “Becoming Agile in an Imperfect World”, and is a seasoned speaker who has made several keynote appearances worldwide.
+                He is the co-author of <em>Becoming Agile in an Imperfect World,</em> and is a seasoned speaker who has made several keynote appearances worldwide.
+              </p>
+            </read-more>
+          </div>
+        </div>
+
+        <div class="row text-white py-2" id="speakerEmCampbellPretty">
+          <div class="col-12 col-md-3">
+            <div class="img-wrap-ac-speaker-port m-auto">
+              <img class="img-fluid img-brighten-110" src="~assets/images/speaker-em-campbell-pretty-port.jpg" alt="Em Campbell-Pretty">
+            </div>
+          </div>
+          <div class="col-12 col-md-9 mt-2 text-center text-md-left">
+            <h4 class="mt-2 mt-md-0">Em Campbell-Pretty</h4>
+            <p class="body-copy">
+              Em Campbell-Pretty is the author of the Amazon Number #1 Best Seller: <em>Tribal Unity.</em>
+              She is an internationally acclaimed business strategist, speaker and one of Australia’s leading Enterprise Agile consultants.
+            </p>
+            <read-more>
+              <p class="body-copy" key="p1">
+                At the heart of Em’s success is her passion for creating cultures of transparency, lean leadership, learning, innovation and fun.
+              </p>
+              <p class="body-copy" key="p2">
+                Em is a SAFe&reg; Fellow and an active member of the global Agile community.
+                She is a thought leader on scaling culture and creating agile tribes.
+                Her commitment to building great leaders and transforming culture has made her a sort after speaker at conferences around the globe.
+              </p>
+            </read-more>
+          </div>
+        </div>
+
+        <div class="row text-white py-2" id="speakerDavidMarquet">
+          <div class="col-12 col-md-3">
+            <div class="img-wrap-ac-speaker-port m-auto">
+              <img class="img-fluid" src="~assets/images/speaker-david-marquet-port.jpg" alt="David Marquet">
+            </div>
+          </div>
+          <div class="col-12 col-md-9 mt-2 text-center text-md-left">
+            <h4 class="mt-2 mt-md-0">David Marquet</h4>
+            <p class="body-copy">
+              David Marquet is an expert on leadership and organizational design, former submarine commander, and author of Amazon #1 Best Seller: <em>Turn the Ship Around!</em> and <em>Turn Your Ship Around!</em>
+            </p>
+            <read-more>
+              <p class="body-copy" key="p1">
+                David Marquet imagines a work place where everyone engages and contributes their full intellectual capacity, a place where people are healthier and happier because they have more control over their work &mdash; a place where everyone is a leader.
+              </p>
+              <p class="body-copy" key="p2">
+                A 1981 U.S. Naval Academy graduate, Captain Marquet served in the U.S. submarine force for 28 years.
+                After being assigned to command the nuclear powered submarine USS Santa Fe &mdash; then ranked last in retention and operational standing &mdash; he realized the traditional leadership approach of &ldquo;take control, give orders,&rdquo; wouldn’t work.
+                He &ldquo;turned the ship around&rdquo; by treating the crew as leaders, not followers, and giving control, not taking control.
+                This approach took the Santa Fe from &ldquo;worst to first,&rdquo; achieving the highest retention and operational standings in the navy.
+              </p>
+              <p class="body-copy" key="p3">
+                After Captain Marquet’s departure, the Santa Fe continued to win awards and promoted a disproportionate number of officers and enlisted men to leadership positions, including ten subsequent submarine captains.
+                Stephen R. Covey said it was the most empowering organization he’d ever seen and wrote about Captain Marquet’s leadership practices in his book, <em>The 8th Habit</em>.
+              </p>
+              <p class="body-copy" key="p4">
+                Captain Marquet is the author of <em>Turn the Ship Around! A True Story of Turning Followers Into Leaders.</em> Fortune magazine named it the #1 must-read business book of the year, and USA Today listed it as one of the top 12 business books of all time.
+                He is also the author of <em>Turn Your Ship Around! A workbook for implementing Intent-Based Leadership.</em>
+              </p>
+              <p class="body-copy" key="p5">
+                Captain Marquet retired from the Navy in 2009, and now speaks to audiences around the globe who want to create empowering work environments that release the passion, initiative, and intellect of each person.
+                This bold and highly effective framework is summarized as &ldquo;give control, create leaders.&rdquo;
+                He is a life member of the Council on Foreign Relations, and in 2015 was named to the American Management Association’s &ldquo;Leaders to Watch&rdquo; list.
               </p>
             </read-more>
           </div>
@@ -170,8 +233,12 @@
             <h3><span class="badge badge-default">Bronze</span></h3>
           </div>
 
-          <div class="col-12 col-sm-6 offset-sm-1 d-flex flex-row flex-nowrap align-items-center">
+          <div class="col-12 col-sm-4 d-flex flex-row flex-nowrap align-items-center">
             <a href="http://www.thei4group.com" target="_blank" class="mx-2 my-4"><img class="img-fluid" src="~assets/images/sponsor-i4group.png" /></a>
+          </div>
+
+          <div class="col-12 col-sm-4 d-flex flex-row flex-nowrap align-items-center">
+            <a href="http://scottambler.com" target="_blank" class="mx-2 my-4"><img class="img-fluid" src="~assets/images/sponsor-scottambler.png" /></a>
           </div>
         </div>
       </section>
