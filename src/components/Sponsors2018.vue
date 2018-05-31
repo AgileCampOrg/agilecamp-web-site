ee89504c9db3c9ca2cc6a60f57877855962caaed
J. Scott Smith <scott@newleafsolutionsinc.com>
2018-05-31 10:45:27 -0700

    Added sponsors to 2018

--- src/components/Sponsors2018.vue
+++ src/components/Sponsors2018.vue
@@ -13,11 +13,23 @@
     </div>
 
     <div class="col-6 col-md-3 col-lg-2 px-4 py-4 d-flex flex-row flex-nowrap align-items-center">
+      <a href="https://icagile.com" target="_blank"><img class="img-fluid" src="~assets/images/sponsor-ica-white.png" /></a>
+    </div>
+
+    <div class="col-6 col-md-3 col-lg-2 px-4 py-4 d-flex flex-row flex-nowrap align-items-center">
       <a href="http://improving.com" target="_blank"><img class="img-fluid" src="~assets/images/sponsor-improving-white.png" /></a>
     </div>
 
     <div class="col-6 col-md-3 col-lg-2 px-4 py-4 d-flex flex-row flex-nowrap align-items-center">
+      <a href="https://www.matrixres.com" target="_blank"><img class="img-fluid" src="~assets/images/sponsor-matrix-white.png" /></a>
+    </div>
+
+    <div class="col-6 col-md-3 col-lg-2 px-4 py-4 d-flex flex-row flex-nowrap align-items-center">
       <a href="https://www.infoq.com" target="_blank"><img class="img-fluid" src="~assets/images/sponsor-infoq-white.png" /></a>
+    </div>
+
+    <div class="col-6 col-md-3 col-lg-2 px-4 py-4 d-flex flex-row flex-nowrap align-items-center">
+      <a href="http://www.eliassen.com/agile/" target="_blank"><img class="img-fluid" src="~assets/images/sponsor-eliassengroup-white.png" /></a>
     </div>
 
     <div class="col-6 col-md-3 col-lg-2 px-4 py-4 d-flex flex-row flex-nowrap align-items-center">
