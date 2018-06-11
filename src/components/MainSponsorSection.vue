72bfd224d0e1c882afd1d7a7ef8859d41ba4ebf8
J. Scott Smith <scott@newleafsolutionsinc.com>
2018-06-10 23:54:31 -0700

    Tweaked layout of title sponsors

--- src/components/MainSponsorSection.vue
+++ src/components/MainSponsorSection.vue
@@ -2,19 +2,19 @@
   <div class="bg-color-red">
     <section class="container">
       <div class="row py-3">
-        <div class="col-6 col-md-3 d-flex flex-row flex-nowrap align-items-center">
+        <div class="col-6 col-md-3 col-xl-2 offset-xl-2 d-flex flex-row flex-nowrap align-items-center">
           <a href="http://nike.com" target="_blank"><img class="img-fluid" src="~assets/images/sponsor-nike-white.png" /></a>
         </div>
 
-        <div class="col-6 col-md-3 d-flex flex-row flex-nowrap align-items-center">
+        <div class="col-6 col-md-3 col-xl-2 d-flex flex-row flex-nowrap align-items-center">
           <a href="https://www.prudential.com" target="_blank"><img class="img-fluid" src="~assets/images/sponsor-prudential-white.png" /></a>
         </div>
 
-        <div class="col-6 col-md-3 d-flex flex-row flex-nowrap align-items-center">
+        <div class="col-6 col-md-3 col-xl-2 d-flex flex-row flex-nowrap align-items-center">
           <a href="http://hyperdriveagile.com" target="_blank"><img class="img-fluid" src="~assets/images/sponsor-hyperdrive-white.png" /></a>
         </div>
 
-        <div class="col-6 col-md-3 px-4 d-flex flex-row flex-nowrap align-items-center">
+        <div class="col-6 col-md-3 col-xl-2 px-4 d-flex flex-row flex-nowrap align-items-center">
           <div class="d-flex flex-column flex-nowrap align-items-center">
             <a href="https://icagile.com" target="_blank"><img class="img-fluid" src="~assets/images/sponsor-ica-white.png" /></a>          
             <span class="small text-white text-center">Official Education Sponsor</span>
