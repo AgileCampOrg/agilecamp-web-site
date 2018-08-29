6b3f2dbe2ef3df1636ae284822a358de65c5d538
jscottsf <>
2018-08-28 17:32:36 -0700

    Added sponsor CollabNet

--- src/components/Sponsors2018.vue
+++ src/components/Sponsors2018.vue
@@ -53,7 +53,13 @@
 
       <div class="col-4 col-md-2 px-4 py-4 d-flex flex-row flex-nowrap justify-content-center align-items-center">
         <a href="https://www.scrumalliance.org" target="_blank" class="text-center">
-          <img src="~assets/images/sponsor-scrumalliance-white.png" style="width: 100%;" />
+          <img src="~assets/images/sponsor-scrumalliance-white.png" style="width: 90%;" />
+        </a>
+      </div>
+
+      <div class="col-4 col-md-2 px-4 py-4 d-flex flex-row flex-nowrap justify-content-center align-items-center">
+        <a href="https://www.collab.net" target="_blank" class="text-center">
+          <img src="~assets/images/sponsor-collabnet-vo-white.png" style="width: 100%;" />
         </a>
       </div>
 
