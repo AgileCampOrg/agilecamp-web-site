549307cf5cf5ff0a1832c893805383c77d15e336
J. Scott Smith <scott@newleafsolutionsinc.com>
2019-04-18 10:20:04 -0700

    Added new sponsors, updated HD logo

--- src/components/Sponsors2019.vue
+++ src/components/Sponsors2019.vue
@@ -8,30 +8,36 @@
 
     <div class="row justify-content-center">
       <scrum-alliance />
+      <certiprof />
       <matrix />
       <improving />
+      <objective-group />
       <i-c-a />
     </div>
   </div>
 </template>
 
 <script>
+import Certiprof from '@/components/sponsors/Certiprof'
 import Hyperdrive from '@/components/sponsors/Hyperdrive'
 import ICA from '@/components/sponsors/ICA'
 import Improving from '@/components/sponsors/Improving'
 import JPMorganChase from '@/components/sponsors/JPMorganChase'
 import Matrix from '@/components/sponsors/Matrix'
 import Nike from '@/components/sponsors/Nike'
+import ObjectiveGroup from '@/components/sponsors/ObjectiveGroup'
 import ScrumAlliance from '@/components/sponsors/ScrumAlliance'
 
 export default {
   components: {
+    Certiprof,
     Hyperdrive,
     ICA,
     Improving,
     JPMorganChase,
     Matrix,
     Nike,
+    ObjectiveGroup,
     ScrumAlliance
   }
 }
--- src/components/sponsors/Hyperdrive.vue
+++ src/components/sponsors/Hyperdrive.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col-4 col-md-2 px-2 py-4 d-flex flex-row flex-nowrap justify-content-center align-items-center">
     <a href="http://hyperdriveagile.com" target="_blank" class="text-center">
-      <img src="~assets/images/sponsor-hyperdrive-white.png" style="width: 90%;" />
+      <img src="~assets/images/sponsor-hyperdrive-white-2019.png" style="width: 50%;" />
     </a>
   </div>
 </template>
